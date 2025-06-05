8d61b425befcd27dbe9da7c762c0a2c6a1acec45
rachitpoudel101 <rachit300poudel@gmail.com>
2025-06-05 22:10:21 +0545

    add Git ignore file

--- login-frontend/src/App.vue
+++ login-frontend/src/App.vue
@@ -1,8 +1,5 @@
-<!-- src/App.vue -->
 <template>
   <div id="app">
-    <!-- Remove or replace <HelloWorld /> if you don’t need it -->
-    <!-- <HelloWorld msg="Welcome" /> -->
     <Signup />
     <UserList />
   </div>
@@ -11,15 +8,13 @@
 <script>
 import Signup from './components/Signup.vue'
 import UserList from './components/UserList.vue'
-// import HelloWorld from './components/HelloWorld.vue'  // 🔴 Remove this line
-// import TheWelcome from './components/TheWelcome.vue'  // Also remove if unused
+
 
 export default {
   components: {
     Signup,
     UserList,
-    // HelloWorld,
-    // TheWelcome
+
   }
 }
 </script>
